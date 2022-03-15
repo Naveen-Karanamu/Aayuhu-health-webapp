f1efa4d53498f15fab3bd45cebceeaa82078248d
Naveen Kumar <karanamunaveen2002@gmail.com>
2022-03-15 23:22:02 +0530

    Updated: Navbar

--- front/src/Components/Navbar/Default.Navbar.jsx
+++ front/src/Components/Navbar/Default.Navbar.jsx
@@ -8,14 +8,14 @@ import { FaHeartbeat } from 'react-icons/fa';
 const Mobile = () => {
     return (
         <>
-            <div className='flex items-center justify-between bg-blue-200 py-3 px-3'>
+            <div className='flex items-center justify-between bg-blue-400 py-3 px-3'>
                 <div className='flex items-center gap-3 '>
                     <FaHeartbeat className='w-10 h-10 text-red-500' />
                     <h1 className='text-lg font-semibold'>PARK CAPSULE</h1>
                 </div>
                 <div className='flex items-center gap-1'>
-                    <BsFillLockFill className='text-blue-700 w-6 h-6' />
-                    <h4 className='text-blue-700 font-bold text-lg'>Login</h4>
+                    <BsFillLockFill className='text-white w-6 h-6' />
+                    <h4 className='text-white font-bold text-lg'>Login</h4>
                 </div>
             </div>
         </>
@@ -25,7 +25,7 @@ const Mobile = () => {
 const Medium = () => {
     return (
         <>
-            <div className=' md:container md:py-4 md:px-6 md:flex md:items-center md:justify-between   bg-blue-200'>
+            <div className=' md:container md:py-4 md:px-6 md:flex md:items-center md:justify-between   bg-blue-400'>
                 <div className='md:flex md:items-center md:gap-4'>
                     <div className=''>
                         <FaHeartbeat className='w-10 h-10 text-red-500' />
@@ -43,8 +43,8 @@ const Medium = () => {
 
 
                 <div className='md:flex md:items-center md:gap-2'>
-                    <BsFillLockFill className='text-blue-700 w-6 h-6' />
-                    <h4 className='text-blue-700 font-bold text-lg'>Login</h4>
+                    <BsFillLockFill className='text-white w-6 h-6' />
+                    <h4 className='text-white font-bold text-lg'>Login</h4>
                 </div>
             </div>
         </>
@@ -53,7 +53,7 @@ const Medium = () => {
 const Mainscreen = () => {
     return (
         <>
-            <div className=' lg:container lg:py-4 lg:px-24 lg:flex lg:items-center lg:justify-between   bg-blue-200'>
+            <div className=' lg:container lg:py-4 lg:px-24 lg:flex lg:items-center lg:justify-between   bg-blue-400'>
                 <div className='lg:flex lg:items-center lg:gap-4'>
                     <div className=''>
                         <FaHeartbeat className='w-12 h-12 text-red-500' />
@@ -71,8 +71,8 @@ const Mainscreen = () => {
 
 
                 <div className='lg:flex lg:items-center lg:gap-4'>
-                    <BsFillLockFill className='text-blue-700 w-8 h-8' />
-                    <h4 className='text-blue-700 font-bold text-lg'>Login</h4>
+                    <BsFillLockFill className='text-white w-8 h-8' />
+                    <h4 className='text-white font-bold text-lg'>Login</h4>
                 </div>
             </div>
 
