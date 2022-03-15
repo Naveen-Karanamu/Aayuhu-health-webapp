ee5fe48e682ade67289692bb5a70411b69a2f885
Naveen Kumar <karanamunaveen2002@gmail.com>
2022-03-15 23:54:47 +0530

    Built: Homepage's main section

--- front/src/Components/pages/Home.page.jsx
+++ front/src/Components/pages/Home.page.jsx
@@ -22,6 +22,22 @@ const HomePage = () => {
 
                 </div>
 
+                <div className=" md:flex md:px-8 md:justify-between md:items-center lg:container lg:mx-auto lg:px-16 lg:gap-10 lg:py-6 ">
+                    <div className="flex flex-col gap-4 lg:w-1/2 lg:gap-10">
+                        <div className="flex items-center flex-col gap-1 md:items-start lg:gap-3">
+                            <h1 className="text-2xl lg:text-4xl md:font-bold font-semibold">ONE INDIA MANY HOSPITAL'S</h1>
+                            <h5 className="text-xl font-normal lg:font-semibold lg:text-2xl">Choose your Hospital</h5>
+                        </div>
+                        <div className="flex items-center gap-4 justify-center md:justify-start lg:gap-6 px-6 md:px-0">
+                            <button className="bg-blue-400 px-4 py-2 rounded-lg font-bold text-white text-md lg:text-xl lg:px-6 lg:py-3 w-full">Book Slot</button>
+                            <button className="bg-blue-400 px-4 py-2 rounded-lg font-bold text-white text-md lg:text-xl lg:px-6 lg:py-3 w-full">Fix Slot</button>
+                        </div>
+                    </div>
+                    <div className="p-6 rounded-lg md:p-0 md:w-80 lg:w-1/2">
+                        <img src="https://t4.ftcdn.net/jpg/02/74/73/01/360_F_274730119_ht4FXz4R6RnIJgPk7WeNALxxaf524Jrb.jpg" alt="doctors" className="w-full h-full rounded-lg" />
+                    </div>
+                </div>
+
             </div>
         </>
     )
