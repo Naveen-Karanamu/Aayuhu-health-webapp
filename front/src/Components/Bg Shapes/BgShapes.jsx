cf18ce2e0c8b4dfbaacb54b2fcf36f695f3292ae
Naveen Kumar <karanamunaveen2002@gmail.com>
2022-03-16 12:10:16 +0530

    Updated: BgShape component

--- front/src/Components/Bg Shapes/BgShapes.jsx
+++ front/src/Components/Bg Shapes/BgShapes.jsx
@@ -4,11 +4,11 @@ const BgShape = () => {
     return (
         <>
             <div>
-                <div className="fixed -z-10 opacity-20 -right-96 -bottom-20 rotate-90" style={{ width: "700px" }}>
-                    <img src="https://www.norderbergs.com/storage/2020/09/background-shapes-png-1-Images-PNG-Transparent.png" className="w-full h-full" />
+                <div className="fixed -z-10 opacity-20 md:-right-96 lg:-bottom-48 md:top-4 top-72 rotate-90 lg:w-full -right-80" style={{ width: "700px" }}>
+                    <img src="https://www.norderbergs.com/storage/2020/09/background-shapes-png-1-Images-PNG-Transparent.png" className="md:w-full md:h-full w-96 h-80" />
                 </div>
-                <div className="fixed -z-10 opacity-20 -left-96 -top-20 -rotate-180" style={{ width: "700px" }}>
-                    <img src="https://www.norderbergs.com/storage/2020/09/background-shapes-png-1-Images-PNG-Transparent.png" className="w-full h-full" />
+                <div className="fixed -z-10 opacity-20 md:-left-96 right-56 lg:-top-12 lg:bottom-64 md:bottom-12 bottom-10 -rotate-180 lg-w-full " style={{ width: "700px" }}>
+                    <img src="https://www.norderbergs.com/storage/2020/09/background-shapes-png-1-Images-PNG-Transparent.png" className="md:w-full md:h-full w-96 h-80" />
                 </div>
             </div>
         </>
