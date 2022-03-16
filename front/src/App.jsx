477b7cc638f229e7ef351d121cedc0ce914d0a01
Naveen Kumar <karanamunaveen2002@gmail.com>
2022-03-16 10:33:52 +0530

    Setup: Dashboard page

--- front/src/App.jsx
+++ front/src/App.jsx
@@ -1,13 +1,16 @@
 // HOC
 import HomeHOC from "./HOC/Home.HOC";
+import DashboardHOC from "./HOC/Dashboard.HOC";
 
 // Pages
 import HomePage from "./Components/pages/Home.page";
+import DashboardPage from "./Components/pages/Dashboard.page";
 
 function App() {
   return (
     <>
       <HomeHOC path="/home" exact component={HomePage} />
+      <DashboardHOC path="/dashboard" exact component={DashboardPage}/>
     </>
   );
 }
