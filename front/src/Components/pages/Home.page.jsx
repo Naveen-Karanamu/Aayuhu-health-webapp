cb637f5e08349cb7b528a112c1f37a7aa22ca89f
Naveen Kumar <karanamunaveen2002@gmail.com>
2022-03-16 10:26:05 +0530

    Updated: Changes

--- front/src/Components/pages/Home.page.jsx
+++ front/src/Components/pages/Home.page.jsx
@@ -1,25 +1,14 @@
 import React from "react";
 
-// Icons
-import { IoLocationSharp } from "react-icons/io5"
-import { RiArrowDropDownFill } from "react-icons/ri"
+// Components
+import SearchBar from "../Search Bar/SearchBar";
 
 const HomePage = () => {
     return (
         <>
             <div className="lg:container lg:mx-auto lg:px-40 lg:pb-8 pt-16 lg:pt-24 md:pt-20">
-                <div className="lg:flex lg:justify-center lg:m-0  py-6">
-                    <div className="flex md:gap-6  gap-2 justify-center bg-blue-100 lg:w-3/4 lg:py-2 lg:px-8 items-center rounded-lg lg:mx-10 px-2 py-2 mx-4">
-                        <div className="flex items-center gap-2 lg:gap-4 w-1/3 border-r-2 border-black">
-                            <span><IoLocationSharp className="md:w-6 md:h-6 w-4 h-4" /></span>
-                            <span>Chennai</span>
-                            <span className="md:pl-20 "><RiArrowDropDownFill className="md:w-8 md:h-8 w-8 h-8" /></span>
-                        </div>
-                        <div className="w-2/3">
-                            <input type="search" placeholder="Search your requirements" className=" bg-white text-blue-800 w-full lg:p-3 rounded-lg text-center p-2 focus:outline-none" />
-                        </div>
-                    </div>
-
+                <div>
+                    <SearchBar />
                 </div>
 
                 <div className=" md:flex md:px-8 md:justify-between md:items-center lg:container lg:mx-auto lg:px-16 lg:gap-10 lg:py-6 ">
--- front/src/Mobile Navigator/MobileNavigator.jsx
+++ front/src/Mobile Navigator/MobileNavigator.jsx
@@ -9,10 +9,10 @@ const MobileNavigator =()=>{
     return(
         <>
             <div className="md:hidden fixed bottom-0 bg-gray-300 w-full pb-1 pt-2 flex justify-between px-4">
-                <span className="flex flex-col items-center text-gray-700"><AiFillHome className="w-7 h-7 "/>Home</span>
-                <span className="flex flex-col items-center text-gray-700"><FaMicroscope className="w-7 h-7 "/>Features</span>
-                <span className="flex flex-col items-center text-gray-700"><AiFillInfoCircle className="w-7 h-7 "/>About</span>
-                <span className="flex flex-col items-center text-gray-700"><BsFillTelephoneOutboundFill className="w-6 h-7 "/>Contact</span>
+                <span className="flex flex-col items-center text-gray-700"><AiFillHome className="w-6 h-6 "/>Home</span>
+                <span className="flex flex-col items-center text-gray-700"><FaMicroscope className="w-6 h-6 "/>Features</span>
+                <span className="flex flex-col items-center text-gray-700"><AiFillInfoCircle className="w-6 h-6 "/>About</span>
+                <span className="flex flex-col items-center text-gray-700"><BsFillTelephoneOutboundFill className="w-5 h-6 "/>Contact</span>
             </div>
         </>
     )
