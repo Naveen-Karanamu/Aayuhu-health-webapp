9ee08ba1968b1603ce6d434954237a15e70d55a1
Naveen Kumar <karanamunaveen2002@gmail.com>
2022-03-18 10:52:40 +0530

    Built: Searching of doctors

--- front/src/App.jsx
+++ front/src/App.jsx
@@ -5,16 +5,17 @@ import { Route, Redirect } from "react-router-dom";
 import HomeHOC from "./HOC/Home.HOC";
 import DashboardHOC from "./HOC/Patient.Dashboard.HOC";
 import RequestHOC from "./HOC/Request.HOC";
+import SearchingHOC from "./HOC/Searching.HOC";
 
 // Pages
 import HomePage from "./Components/pages/Home.page";
 import DashboardPage from "./Components/pages/Patient.Dashboard.page";
 import DoctorDashboardHOC from "./HOC/Doctor.Dashboard.HOC";
 import DoctorDashboardPage from "./Components/pages/Doctor.Dashboard";
-import Requestpage from "./Components/pages/Request.page";
 import PastRequests from "./Components/pages/Requests/Past.Requests";
 import UpcomingRequests from "./Components/pages/Requests/Upcoming.Requests";
 import RequestsRequests from "./Components/pages/Requests/Requests.Requests";
+import { Searchingpage } from "./Components/pages/Searching.page";
 
 function App() {
   return (
@@ -31,6 +32,7 @@ function App() {
       <RequestHOC path="/request/:id/past" exact component={PastRequests}/>
       <RequestHOC path="/request/:id/upcoming" exact component={UpcomingRequests}/>
       <RequestHOC path="/request/:id/requests" exact component={RequestsRequests}/>
+      <SearchingHOC path="/search" exact component={Searchingpage}/>
     </>
   );
 }
